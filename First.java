0c529730f2ea7dcf7f1c0d23e6f54dccab4a577a
Святослав <126155077+sviatoslavlisovets@users.noreply.github.com>
2023-02-28 23:37:53 +0200

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+public class First {
+    public void dowork(){
+        System.out.println();
+        int a = 10;
+        int b = 3;
+        int result = a / b;
+        int remainder = a % b;
+
+        System.out.println("Result: " + result);
+        System.out.println("Remainder: " + remainder);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        First first = new First();
+        first.dowork();
+        Second second = new Second();
+        second.dowork();
+        Person person = new Person();
+        person.dowork();
+    }
+}
